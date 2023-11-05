18339b5e75719e43e9fc03e44c9a80dc53f1ae5a
Sree1710 <sreelekshmisl1710@gmail.com>
2023-11-05 14:29:50 +0530

    StudentLogin backend completed

--- App.js
+++ App.js
@@ -11,6 +11,23 @@ app.use(Cors())
 
 mongoose.connect("mongodb+srv://sreelekshmisl1710:Dharithri@cluster0.y83cozw.mongodb.net/collegeDB?retryWrites=true&w=majority", { useNewUrlParser: true })
 
+app.post("/login",async(request,response)=>{
+   let data=request.body
+   let getUsername=data.username
+   let getPassword=data.password
+   let result=await studentModel.find({username:getUsername})
+   if (result.length>0) {
+      if (result[0].password==getPassword) {
+        response.json({"status":"success","data":result[0]})
+      } else {
+        response.json({"status":"Invalid Username or Password !!!"})
+      }
+   } else {
+       response.json({"status":"Username Does Not Exist !!!"})
+   }
+})
+
+
 app.post("/admaddstud", async (request, response) => {
     let data = request.body
     const student = new studentModel(data)
